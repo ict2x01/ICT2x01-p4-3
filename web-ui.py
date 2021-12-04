5c9e33b32205e388e13d35e096012234cd2428bd
Daniel Tan Zhonghao <33278298+danieltannn@users.noreply.github.com>
2021-12-04 16:39:28 +0800

    Delete web-ui.py

--- web-ui.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# Decorator that turns a regular Python function into a Flask view function
-@app.route('/')
-def index():
-    return render_template('index.html')
