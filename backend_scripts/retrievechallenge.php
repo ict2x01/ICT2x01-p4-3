b2d0833206ca9b5b424d1509bcba5df5baba786f
Daniel Tan Zhonghao <33278298+danieltannn@users.noreply.github.com>
2021-12-06 02:42:09 +0800

    select challenge feature
    Allow students to select challenge from a list of challenges queried from db

---
+++ backend_scripts/retrievechallenge.php
@@ -0,0 +1,24 @@
+<?php
+
+function retrieveAllChallenges(){
+    $db_filepath = "../../challenges.db";
+    $db = new SQLite3($db_filepath);
+    $sql_stmt = "SELECT * from challenges";
+    $prepared_stmt = $db->prepare($sql_stmt);
+    $res = $prepared_stmt->execute();
+
+    return $res; //return all in db
+}
+
+function takeChallenge($id){
+    $db_filepath = "../../challenges.db";
+    $db = new SQLite3($db_filepath);
+
+    $sql_stmt = "SELECT * from challenges WHERE id=:id";
+    $prepared_stmt = $db->prepare($sql_stmt); //prepare the statement
+    $prepared_stmt->bindParam(":id", $id);
+    $res = $prepared_stmt->execute();//execute statement
+    
+    return $res->fetchArray(SQLITE3_ASSOC)["map"]; //return one row based on id
+}
+?>
