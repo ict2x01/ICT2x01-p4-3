606c81139d76150c67937d94751dca0726d20239
Shannen Wee <74093766+shannenwee@users.noreply.github.com>
2021-12-05 23:47:32 +0800

    Add files via upload

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -8,11 +8,28 @@
     <body>
         <main class="box">
             <h1>Dashboard</h1>
-            <br><br>
             <p>View statistics here.</p>
-            <br><br>
-            <!-- Placeholder, insert graph here when done -->
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdt5Xx0rfq4FH_kdoZRbj2sd1_KDM1kq9Jj5bHjM2e6CF9VGXl9MyBQlDHc-ep2WVZitI&usqp=CAU">
+            <div class="row">
+                <div class="col-sm">
+                    <br>
+                    <h3>Game Statistics</h3>
+                    <p>Total Hours Spent Playing:</p>
+                    <p>Total Games Played:</p>
+                    <p>Games Won:</p>
+                    <p>Winning %:</p>
+                </div>
+                <div class="col-sm">
+                    <br>
+                    <h3>Car Statistics</h3>
+                    <p>Total Distance Travelled:</p>
+                    <p>Average Speed:</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <img src="https://cnx.org/resources/fff772d27029cb8e720c8be9db336c968f835d22/fig-ch12_14_01.png">
+                </div>
+            </div>
         </main>
     </body>
 </html>
