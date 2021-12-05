a16cddedfc3734ee38351738d8b4b59db09ef78e
Daniel Tan Zhonghao <33278298+danieltannn@users.noreply.github.com>
2021-12-06 02:48:27 +0800

    Starting page of website

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+            include "templates/head.html";
+        ?>
+    </head>
+    <body>
+        <main class="box ctr">
+            <h1>CarKART</h1>
+            <br><br>
+            <img src="images/car.jpg" width="384" height="216">
+            <br><br>
+            <a class="btn btn-primary" href="templates/playpage.php" role="button">PLAY!</a>
+            <br><br>
+            <a class="btn btn-success" href="templates/dashboard.php" role="button">Dashboard</a>
+            <br><br><br><br>
+            <a class="btn btn-info" href="templates/login.php" style="float: right" role="button">Trainer Login</a>
+        </main>
+    </body>
+</html>
