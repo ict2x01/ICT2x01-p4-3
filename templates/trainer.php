8f99a4d294b229125d5eb735e1a69a0747be7ca7
Daniel Tan Zhonghao <33278298+danieltannn@users.noreply.github.com>
2021-12-06 02:54:11 +0800

    trainer page

---
+++ templates/trainer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+            include "head.html";
+        ?>
+    </head>
+    <body>
+        <main class="box ctr">
+            <h1>Trainer Interface</h1>
+            <br>
+            <h2>Welcome, Trainer!</h2>
+            <p>Here you can monitor students' progress and peform other additional tasks.</p>
+        </main>
+    </body>
+</html>
