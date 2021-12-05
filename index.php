bf83bdafb689194462a990a75bd6cb17aa08a0c4
Shannen Wee <74093766+shannenwee@users.noreply.github.com>
2021-12-05 21:42:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+            include "head.html";
+        ?>
+    </head>
+    <body>
+        <main class="box ctr">
+            <h1>CarKART</h1>
+            <br><br>
+            <img src="../images/car.jpg" width="384" height="216">
+            <br><br>
+            <a class="btn btn-primary" href="playpage.php" role="button">PLAY!</a>
+            <br><br>
+            <a class="btn btn-info" href="instructions.html" role="button">Instructions</a>
+            <br><br>
+            <a class="btn btn-success" href="dashboard.php" role="button">Dashboard</a>
+            <br><br>
+            <a class="btn btn-secondary" href="history.html" role="button">History</a>
+            <br><br><br><br>
+            <a class="btn btn-secondary" href="login.php" style="float: right" role="button">Trainer Login</a>
+        </main>
+    </body>
+</html>
